f6da79ae815afbf362c5a09add57e9f1b28930f0
manuelba1997 <manuelbrugues@gmail.com>
2024-04-26 20:07:56 -0400

    restructuring the project, adding Home and AboutUs

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import NavBar from './componet/NavBar';
 import MainContent from './componet/MainContent';
+import Home from './componet/Home';
 import Footer from './componet/Footer';
 import OurProductContainer from './componet/OurProductsContainer';
 function App() {
@@ -8,7 +9,7 @@ function App() {
   return (
     <div className="App">
          <NavBar />
-         <MainContent />
+         <Home />
          <OurProductContainer/>
          <Footer />
         
---
+++ firstapp/src/componet/Home.js
@@ -0,0 +1,15 @@
+import MainContent from './MainContent';
+
+
+
+export default function Home(){
+
+
+
+
+    return(
+        <div>
+          <MainContent></MainContent>
+          </div>
+    )
+}
